8b134dd9dbf216562e46e08fdd9f2a4f8776f721
Sara Westerberg <sara.westerberg8@campusnykoping.se>
2025-11-24 14:46:31 +0100

    added testproject

---
+++ TrainingTracker.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TrainingTracker.Tests
+{
+    public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(Microsoft.AspNetCore.Hosting.IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Here we can customize the services for testing, e.g., use an in-memory database
+
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(Microsoft.EntityFrameworkCore.DbContextOptions<TrainingTrackerAPI.Data.ApplicationDbContext>));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                //Test database connection
+                var dbConnectionDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(SqlConnection));
+
+                if (dbConnectionDescriptor != null)
+                {
+                    services.Remove(dbConnectionDescriptor);
+                }
+
+                services.AddSingleton<SqlConnection>(container =>
+                {
+                    var connection = new SqlConnection("DataSource=:memory:");
+                    connection.Open();
+                    return connection;
+                });
+
+                services.AddDbContext<TrainingTrackerAPI.Data.ApplicationDbContext>((container, options) =>
+                {
+                    var connection = container.GetRequiredService<SqlConnection>();
+                    options.UseSqlServer(connection);
+
+                });
+
+            });
+        }
+
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            var host = base.CreateHost(builder);
+            using var scope = host.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<TrainingTrackerAPI.Data.ApplicationDbContext>();
+            db.Database.EnsureCreated();
+            return host;
+        }
+    }
+}
