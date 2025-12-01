2e513391a470f09930c27dd006e6e1583a9b033a
Felicia Runfrost <Felicia.Runfrost@hotmail.com>
2025-12-01 15:45:49 +0100

    Add year of birth selection and update user settings
    Updated `Settings.cshtml` to include a dropdown for year of birth and a submit button. Removed the hidden input for the user's name and added validation messages.
    
    Introduced `YearOptions` property and `BuildYearOptions` method in `Settings.cshtml.cs` to populate the year dropdown. Modified `OnGetAsync` to ensure the dropdown is populated on page load and enhanced `OnPostAsync` for form submission with validation and error handling.
    
    Updated `settings.css` to style the new settings button with hover effects, improving the user interface.

--- TrainingTracker/Pages/Settings.cshtml.cs
+++ TrainingTracker/Pages/Settings.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using TrainingTracker.DAL;
 using TrainingTrackerAPI.DTO;
 using TrainingTrackerAPI.Models;
@@ -15,11 +16,29 @@ namespace TrainingTracker.Pages
             _userManager = userManager;
         }
 
+        [BindProperty]
         public UserSettingsDto UserSettings { get; set; }
+        public List<SelectListItem> YearOptions { get; set; }
+       
+        public void BuildYearOptions()
+        {
+            var currentYear = DateTime.UtcNow.Year;
+            YearOptions = new List<SelectListItem>();
+
+            for (int year = currentYear; year >= (currentYear - 90); year--)
+            {
+                YearOptions.Add(new SelectListItem
+                {
+                    Value = year.ToString(),
+                    Text = year.ToString()
+                });
+            }
+        }
         public async Task OnGetAsync()
         {
             if (User.Identity != null && User.Identity.IsAuthenticated)
             {
+                BuildYearOptions();
                 var user = await _userManager.GetUserAsync(User);
                 UserSettings = new UserSettingsDto
                 {
@@ -33,6 +52,46 @@ namespace TrainingTracker.Pages
             {
                 UserSettings = null;
             }
+
+           
+
+
         }
+
+        public async Task<IActionResult> OnPostAsync(UserSettingsDto userSettings)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                BuildYearOptions();     
+                return Page();
+            }
+            if (User.Identity != null && User.Identity.IsAuthenticated)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user != null)
+                {
+                    user.Name = userSettings.Name;
+                    user.YearOfBirth = userSettings.YearOfBirth;
+                    user.Weight = userSettings.Weight;
+                    user.Height = userSettings.Height;
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToPage();
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return Page();
+        }
+
+        
     }
 }
